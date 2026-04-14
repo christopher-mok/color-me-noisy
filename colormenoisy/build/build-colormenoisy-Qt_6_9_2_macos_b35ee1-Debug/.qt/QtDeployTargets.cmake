d8d2530151243696c36022d41463611f18453655
BCW1 <BCW1@chae-prime-2.mynetworksettings.com>
2026-04-13 23:33:04 -0400

    pyramid

--- colormenoisy/build/build-colormenoisy-Qt_6_9_2_macos_b35ee1-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_colormenoisy_FILE /Users/BCW1/Desktop/College/CS/CS2240/color-me-noisy/colormenoisy/build/build-colormenoisy-Qt_6_9_2_macos_b35ee1-Debug/colormenoisy.app/Contents/MacOS/colormenoisy)
-set(__QT_DEPLOY_TARGET_colormenoisy_TYPE EXECUTABLE)

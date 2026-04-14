0585f81744ad1bd40e895c9837a63515896c28a6
BCW1 <BCW1@chae-prime-2.mynetworksettings.com>
2026-04-13 23:31:28 -0400

    image pyramid

--- image_pyramid.h
+++ image_pyramid.h
@@ -1,10 +1,19 @@
 #ifndef IMAGE_PYRAMID_H
 #define IMAGE_PYRAMID_H
 
-class image_pyramid
+#include "image_utils.h"
+
+
+class ImagePyramid
 {
 public:
-    image_pyramid();
+    ImagePyramid();
+    std::vector<Image> make_gaussian_pyramid(Image im);
+
+private:
+    Image upsample(const Image img);
+    RGB blur(const Image& img, int x, int y);
+    Image downsample(const Image img);
 };
 
 #endif // IMAGE_PYRAMID_H
